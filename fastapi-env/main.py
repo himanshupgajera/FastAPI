6080a7086fc32e44a8392fd4a1e9ea25357341d0
Himanshu <divine.developer2024@gmail.com>
2025-01-16 16:03:11 +0530

    add post method

--- fastapi-env/main.py
+++ fastapi-env/main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,14 +12,38 @@ async def root():
 # route with parameters
 @app.get("/items")
 async def read_item():
-	return {"message": "Items"}
+	return {"items": "Items"}
 
 # route with id parameters
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
-	return {"message": "Item ID: {}".format(item_id)}
+	return {"items": "Item ID: {}".format(item_id)}
 
 # route with query parameters
 @app.get("/items/{item_id}/{name}")
 async def read_item_name(item_id: int, name: str):
-	return {"message": [{"item_id": item_id, "name": name},{"item_id": item_id, "name": name}]}
+	return {"items": [{"item_id": item_id, "name": name},{"item_id": item_id, "name": name}]}
+
+# query for availabe and not aviailable items
+@app.get("/items")
+async def read_item_name(limit: int, available: bool):
+	if available:
+		return {"message":f'Available items: {limit}'}
+	else:
+		return {"message":f'Not available items: {limit}'}
+
+# add a new item
+@app.post("/products/{item_id}")
+async def create_item(item_id: int):
+	return {"items": f"Item ID: {item_id} added successfully"}
+
+# add a new item with request body
+class Item(BaseModel):
+	name: str
+	description: str
+	price: float
+	tax: float
+
+@app.post("products/name")
+async def add_name(item: Item):
+	return {f"{item.name} added successfully"}
